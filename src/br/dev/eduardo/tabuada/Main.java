9e254de9cc3e466b993cdd469fde037625b4bc85
EDUARDO SANTOS JOSE <25132654@CFP127ALUNO.LOCAL>
2025-04-02 11:22:45 -0300

    inicio de tabuada

---
+++ src/br/dev/eduardo/tabuada/Main.java
@@ -0,0 +1,17 @@
+package br.dev.eduardo.tabuada;
+
+import javax.swing.JFrame;
+
+import model.Tabuada;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		JFrame tela = new JFrame ();
+		tela.setTitle("DS1MA");
+		tela.setSize(500, 600);
+		tela.setVisible(true);
+		
+	
+	}
+}
---
+++ src/model/Tabuada.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Tabuada {
+
+	private double multiplicando;
+	private double minimoMultiplicador;
+	private double maximoMultiplicador;
+
+	public double getMultiplicando() {
+		return multiplicando;
+	}
+
+	public void setMultiplicando(double multiplicando) {
+		this.multiplicando = multiplicando;
+	}
+
+	public double getMinimoMultiplicador() {
+		return minimoMultiplicador;
+	}
+
+	public void setMinimoMultiplicador(double minimoMultiplicador) {
+		this.minimoMultiplicador = minimoMultiplicador;
+	}
+
+	public double getMaximoMultiplicador() {
+		return maximoMultiplicador;
+	}
+
+	public void setMaximoMultiplicador(double maximoMultiplicador) {
+		this.maximoMultiplicador = maximoMultiplicador;
+	}
+
+	public void mostrarTabuada() {
+
+		if (minimoMultiplicador > maximoMultiplicador) {
+			double troca = minimoMultiplicador;
+			minimoMultiplicador = maximoMultiplicador;
+			maximoMultiplicador = troca;
+		}
+
+		System.out.println("TABUADA DO " + multiplicando);
+		System.out.println("--------------------------------");
+		while (minimoMultiplicador <= maximoMultiplicador) {
+			double produto = multiplicando * minimoMultiplicador;
+			System.out.println(multiplicando + "X" + minimoMultiplicador + "=" + produto);
+			minimoMultiplicador++;
+		}
+		
+		System.out.println("FIM DA TABUADA DO " + multiplicando);
+		System.out.println("---------------------------------------");
+		System.out.println();
+	}
+}
